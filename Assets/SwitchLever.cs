53193e971c577a74f8b75f7aacb21f1a3c93334c
Noah Vaters <144738466+NoV8rs@users.noreply.github.com>
2025-02-09 23:03:13 -0400

    Changes to the SwitchLever
    The changes to the script makes the interactables spawn in the scene using the lever

--- Assets/SwitchLever.cs
+++ Assets/SwitchLever.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class SwitchLever : XRBaseInteractable
+public class SwitchLever : XRBaseInteractable // Inherit from XRBaseInteractable to use the XR Interaction Toolkit
 {
-    private Quaternion startRotation;
-    private bool isOn = false;
+    private Quaternion startRotation; // The initial rotation of the lever
+    private bool isOn = false; // The state of the lever
+    
+    public GameObject targetObject;
 
     protected override void Awake()
     {
@@ -27,12 +29,16 @@ public class SwitchLever : XRBaseInteractable
         if (isOn)
         {
             // Rotate the lever to the "on" position
-            transform.localRotation = startRotation * Quaternion.Euler(0, 0, -45);
+            transform.localRotation = startRotation * Quaternion.Euler(225, 0, 0);
+            // Activate the target object
+            targetObject.SetActive(true);
         }
         else
         {
             // Rotate the lever back to the "off" position
             transform.localRotation = startRotation;
+            // Deactivate the target object
+            targetObject.SetActive(false);
         }
     }
 }
